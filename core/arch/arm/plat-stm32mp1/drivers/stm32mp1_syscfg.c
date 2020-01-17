zfeifei90/optee_os

commit b3f3564dc4dc438b367e9e25d03a8560242a6824

stm32mp1: deactivate IO compensation in case of STOP mode
Disable IO compensation on A7 side in case of STOP mode.
SYSCFG clock has to be managed at this level, to ensure
access to the SoC interface registers when entering/exiting STOP mode.
This is done in the corresponding low_power service.

This change maps SoC SYSCFG interface registers as non-secure and
defines SYSCFG clock in the clock driver. Without futher change,
shared resource defines the clock as non-secure.

Change-Id: Ie3b075b3bfad45a0f1b489f369bbea4be8766df9
Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/150129
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Etienne CARRIERE <etienne.carriere@st.com>

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_syscfg.c
@@ -1,0 +1,96 @@
+// SPDX-License-Identifier: BSD-3-Clause
+/*
+ * Copyright (c) 2019, STMicroelectronics - All Rights Reserved
+ */
+
+#include <dt-bindings/clock/stm32mp1-clks.h>
+#include <kernel/delay.h>
+#include <stm32_util.h>
+#include <trace.h>
+
+/*
+ * SYSCFG register offsets (base relative)
+ */
+#define SYSCFG_CMPCR				0x20U
+#define SYSCFG_CMPENSETR			0x24U
+
+/*
+ * SYSCFG_CMPCR Register
+ */
+#define SYSCFG_CMPCR_SW_CTRL			BIT(1)
+#define SYSCFG_CMPCR_READY			BIT(8)
+#define SYSCFG_CMPCR_RANSRC			GENMASK_32(19, 16)
+#define SYSCFG_CMPCR_RANSRC_SHIFT		16
+#define SYSCFG_CMPCR_RAPSRC			GENMASK_32(23, 20)
+#define SYSCFG_CMPCR_ANSRC_SHIFT		24
+
+#define SYSCFG_CMPCR_READY_TIMEOUT_US		1000U
+
+/*
+ * SYSCFG_CMPENSETR Register
+ */
+#define SYSCFG_CMPENSETR_MPU_EN			BIT(0)
+
+void stm32mp1_syscfg_enable_io_compensation(void)
+{
+	uintptr_t syscfg_base = stm32_get_syscfg_base();
+	uint64_t start;
+
+	/*
+	 * Activate automatic I/O compensation.
+	 * Warning: need to ensure CSI enabled and ready in clock driver.
+	 * Enable non-secure clock, we assume non-secure is suspended.
+	 */
+	stm32mp1_clk_enable_non_secure(SYSCFG);
+
+	mmio_setbits_32(syscfg_base + SYSCFG_CMPENSETR,
+			SYSCFG_CMPENSETR_MPU_EN);
+
+	start = utimeout_init(SYSCFG_CMPCR_READY_TIMEOUT_US);
+
+	while ((mmio_read_32(syscfg_base + SYSCFG_CMPCR) &
+		SYSCFG_CMPCR_READY) == 0U) {
+		if (utimeout_elapsed(SYSCFG_CMPCR_READY_TIMEOUT_US, start)) {
+			EMSG("IO compensation cell not ready");
+			break;
+		}
+	}
+
+	mmio_clrbits_32(syscfg_base + SYSCFG_CMPCR, SYSCFG_CMPCR_SW_CTRL);
+
+	DMSG("[0x"PRIxPTR"] SYSCFG.cmpcr = 0x"PRIx32,
+	     syscfg_base + SYSCFG_CMPCR,
+	     mmio_read_32(syscfg_base + SYSCFG_CMPCR));
+}
+
+void stm32mp1_syscfg_disable_io_compensation(void)
+{
+	uintptr_t syscfg_base = stm32_get_syscfg_base();
+	uint32_t value;
+
+	/*
+	 * Deactivate automatic I/O compensation.
+	 * Warning: CSI is disabled automatically in STOP if not
+	 * requested for other usages and always OFF in STANDBY.
+	 * Disable non-secure SYSCFG clock, we assume non-secure is suspended.
+	 */
+	value = mmio_read_32(syscfg_base + SYSCFG_CMPCR) >>
+		SYSCFG_CMPCR_ANSRC_SHIFT;
+
+	mmio_clrbits_32(syscfg_base + SYSCFG_CMPCR,
+			SYSCFG_CMPCR_RANSRC | SYSCFG_CMPCR_RAPSRC);
+
+	value = mmio_read_32(syscfg_base + SYSCFG_CMPCR) |
+		(value << SYSCFG_CMPCR_RANSRC_SHIFT);
+
+	mmio_write_32(syscfg_base + SYSCFG_CMPCR, value | SYSCFG_CMPCR_SW_CTRL);
+
+	DMSG("[0x"PRIxPTR"] SYSCFG.cmpcr = 0x"PRIx32,
+	     syscfg_base + SYSCFG_CMPCR,
+	     mmio_read_32(syscfg_base + SYSCFG_CMPCR));
+
+	mmio_clrbits_32(syscfg_base + SYSCFG_CMPENSETR,
+			SYSCFG_CMPENSETR_MPU_EN);
+
+	stm32mp1_clk_disable_non_secure(SYSCFG);
+}

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_clk.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier:	GPL-2.0+	BSD-3-Clause
 /*
- * Copyright (C) 2017-2018, STMicroelectronics - All Rights Reserved
+ * Copyright (C) 2017-2019, STMicroelectronics - All Rights Reserved
  */
 
 #include <assert.h>
@@ -364,6 +364,7 @@ static const struct stm32mp1_clk_gate stm32mp1_clk_gate[] = {
 	_CLK_SELEC(RCC_DBGCFGR, 8, CK_DBG, _UNKNOWN_SEL),
 	_CLK_SC_FIXED(RCC_MP_APB1ENSETR, 6, TIM12_K, _PCLK1),
 	_CLK_SC_FIXED(RCC_MP_APB2ENSETR, 2, TIM15_K, _PCLK2),
+	_CLK_SC_FIXED(RCC_MP_APB3ENSETR, 11, SYSCFG, _UNKNOWN_ID),
 	_CLK_SC_SELEC(RCC_MP_AHB6ENSETR, 0, MDMA, _UNKNOWN_SEL),
 	_CLK_SC_SELEC(RCC_MP_AHB6ENSETR, 5, GPU, _UNKNOWN_SEL),
 	_CLK_SC_FIXED(RCC_MP_AHB6ENSETR, 10, ETHMAC, _ACLK),

core/arch/arm/plat-stm32mp1/drivers/sub.mk
@@ -6,3 +6,4 @@ srcs-$(CFG_STPMIC1) += stm32mp1_pmic.c
 srcs-y += stm32mp1_clk.c
 srcs-$(CFG_DT) += stm32mp1_clkfunc.c
 srcs-y += stm32mp1_ddrc.c
+srcs-y += stm32mp1_syscfg.c

core/arch/arm/plat-stm32mp1/main.c
@@ -47,6 +47,7 @@ register_phys_mem(MEM_AREA_IO_NSEC, RNG1_BASE, SMALL_PAGE_SIZE);
 register_phys_mem(MEM_AREA_IO_NSEC, IWDG1_BASE, SMALL_PAGE_SIZE);
 register_phys_mem(MEM_AREA_IO_NSEC, IWDG2_BASE, SMALL_PAGE_SIZE);
 register_phys_mem(MEM_AREA_IO_NSEC, RTC_BASE, SMALL_PAGE_SIZE);
+register_phys_mem(MEM_AREA_IO_NSEC, SYSCFG_BASE, SMALL_PAGE_SIZE);
 #ifdef CFG_WITH_NSEC_UARTS
 register_phys_mem(MEM_AREA_IO_NSEC, USART1_BASE, SMALL_PAGE_SIZE);
 register_phys_mem(MEM_AREA_IO_NSEC, USART2_BASE, SMALL_PAGE_SIZE);
@@ -422,6 +423,19 @@ uintptr_t stm32_get_stgen_base(void)
 
 	if (!va)
 		va = (uintptr_t)phys_to_virt(STGEN_BASE, MEM_AREA_IO_SEC);
+
+	return va;
+}
+
+uintptr_t stm32_get_syscfg_base(void)
+{
+	static uintptr_t va;
+
+	if (!cpu_mmu_enabled())
+		return SYSCFG_BASE;
+
+	if (!va)
+		va = (uintptr_t)phys_to_virt(SYSCFG_BASE, MEM_AREA_IO_NSEC);
 
 	return va;
 }

core/arch/arm/plat-stm32mp1/platform_config.h
@@ -78,6 +78,7 @@
 #define RTC_BASE			0x5c004000
 #define SPI6_BASE			0x5c001000
 #define STGEN_BASE			0x5C008000
+#define SYSCFG_BASE			0x50020000
 #define TAMP_BASE			0x5c00a000
 #define USART1_BASE			0x5c000000
 #define USART2_BASE			0x4000e000

core/arch/arm/plat-stm32mp1/pm/low_power.c
@@ -170,6 +170,8 @@ int stm32_enter_cstop(uint32_t mode)
 	uintptr_t rcc_base = stm32_rcc_base();
 	int rc;
 
+	stm32mp1_syscfg_disable_io_compensation();
+
 	stm32_apply_pmic_suspend_config(mode);
 
 	if (stm32mp_with_pmic() && (mode == STM32_PM_CSTOP_ALLOW_LP_STOP)) {
@@ -247,6 +249,8 @@ void stm32_exit_cstop(void)
 
 	/* Disable retention and backup RAM content after stop */
 	mmio_clrbits_32(pwr_base + PWR_CR2_OFF, PWR_CR2_BREN | PWR_CR2_RREN);
+
+	stm32mp1_syscfg_enable_io_compensation();
 }
 
 /*

core/arch/arm/plat-stm32mp1/stm32_util.h
@@ -28,6 +28,9 @@ bool stm32_oem_service(struct sm_ctx *ctx,
 /* Platform util for the STGEN driver */
 uintptr_t stm32_get_stgen_base(void);
 
+/* Platform util for the SYSCFG driver */
+uintptr_t stm32_get_syscfg_base(void);
+
 /* Platform util for the GIC */
 uintptr_t get_gicc_base(void);
 uintptr_t get_gicd_base(void);
@@ -118,6 +121,13 @@ void stm32mp_platform_reset(int cpu);
 
 /* Clock calibration */
 int stm32mp_start_clock_calib(unsigned int clock_id);
+
+/*
+ * SYSCFG IO compensation.
+ * These functions assume non-secure world is suspended.
+ */
+void stm32mp1_syscfg_enable_io_compensation(void);
+void stm32mp1_syscfg_disable_io_compensation(void);
 
 /*
  * Shared reference counter: increments by 2 on secure increment

commit 872105f87c6f07d48c6901ea57f1f7bca9858a17

plat-stm32mp1: clk: fix build warning gating of io compensation for pm
Fix build errors reported by GCC:

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_syscfg.c:61:7: error: too many arguments for format [-Werror=format-extra-args]
  DMSG("[0x"PRIxPTR"] SYSCFG.cmpcr = 0x"PRIx32,
       ^~~~~
lib/libutils/ext/include/trace.h:36:8: note: in definition of macro ‘trace_printf_helper’
        __VA_ARGS__)
        ^~~~~~~~~~~

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_syscfg.c:61:2: note: in expansion of macro ‘DMSG’
  DMSG("[0x"PRIxPTR"] SYSCFG.cmpcr = 0x"PRIx32,
  ^~~~

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_syscfg.c: In function ‘stm32mp1_syscfg_disable_io_compensation’:
core/arch/arm/plat-stm32mp1/drivers/stm32mp1_syscfg.c:88:7: error: too many arguments for format [-Werror=format-extra-args]
  DMSG("[0x"PRIxPTR"] SYSCFG.cmpcr = 0x"PRIx32,
       ^~~~~
lib/libutils/ext/include/trace.h:36:8: note: in definition of macro ‘trace_printf_helper’
        __VA_ARGS__)
        ^~~~~~~~~~~
core/arch/arm/plat-stm32mp1/drivers/stm32mp1_syscfg.c:88:2: note: in expansion of macro ‘DMSG’
  DMSG("[0x"PRIxPTR"] SYSCFG.cmpcr = 0x"PRIx32,
  ^~~~

Fixes: b3f3564dc4dc4 ("stm32mp1: deactivate IO compensation in case of STOP mode")
Change-Id: Ia0e66b038939be985d018c75e4aff8e5e0b0b1ef
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/155313
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Lionel DEBIEVE <lionel.debieve@st.com>

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_syscfg.c
@@ -7,6 +7,7 @@
 #include <kernel/delay.h>
 #include <stm32_util.h>
 #include <trace.h>
+#include <types_ext.h>
 
 /*
  * SYSCFG register offsets (base relative)
@@ -58,7 +59,7 @@ void stm32mp1_syscfg_enable_io_compensation(void)
 
 	mmio_clrbits_32(syscfg_base + SYSCFG_CMPCR, SYSCFG_CMPCR_SW_CTRL);
 
-	DMSG("[0x"PRIxPTR"] SYSCFG.cmpcr = 0x"PRIx32,
+	DMSG("[0x%"PRIxPTR"] SYSCFG.cmpcr = 0x%"PRIx32,
 	     syscfg_base + SYSCFG_CMPCR,
 	     mmio_read_32(syscfg_base + SYSCFG_CMPCR));
 }
@@ -85,7 +86,7 @@ void stm32mp1_syscfg_disable_io_compensation(void)
 
 	mmio_write_32(syscfg_base + SYSCFG_CMPCR, value | SYSCFG_CMPCR_SW_CTRL);
 
-	DMSG("[0x"PRIxPTR"] SYSCFG.cmpcr = 0x"PRIx32,
+	DMSG("[0x%"PRIxPTR"] SYSCFG.cmpcr = 0x%"PRIx32,
 	     syscfg_base + SYSCFG_CMPCR,
 	     mmio_read_32(syscfg_base + SYSCFG_CMPCR));
 