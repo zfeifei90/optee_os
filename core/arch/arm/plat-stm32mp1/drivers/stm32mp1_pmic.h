zfeifei90/optee_os

commit 9397a52cb17e636a9744a618c154c05c0161a82b

pmic: stm32mp: export stm32mp_dt_pmic_status()
Add prefix and put stm32mp_dt_pmic_status() available for external usage,
for future developments.

Change-Id: I27957e10936abf77b31fda61adafabb65a71ac3e
Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/150388
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Etienne CARRIERE <etienne.carriere@st.com>
Tested-by: Etienne CARRIERE <etienne.carriere@st.com>

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_pmic.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright (c) 2017-2018, STMicroelectronics - All Rights Reserved
+ * Copyright (c) 2017-2019, STMicroelectronics - All Rights Reserved
  */
 
 #ifndef __STM32MP1_PMIC_H__
@@ -13,6 +13,7 @@ void stm32mp_pmic_apply_boot_on_config(void);
 void stm32mp_pmic_apply_lp_config(const char *lp_state);
 void stm32mp_get_pmic(void);
 void stm32mp_put_pmic(void);
+int stm32mp_dt_pmic_status(void);
 #else
 static inline void stm32mp_pmic_apply_boot_on_config(void)
 {
@@ -27,6 +28,10 @@ static inline void stm32mp_get_pmic(void)
 static inline void stm32mp_put_pmic(void)
 {
 	panic();
+}
+int stm32mp_dt_pmic_status(void)
+{
+	return -1;
 }
 #endif
 

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_pmic.c
@@ -58,7 +58,7 @@ static int dt_get_regulators_node(void *fdt)
 	return fdt_subnode_offset(fdt, node, "regulators");
 }
 
-static int dt_pmic_status(void)
+int stm32mp_dt_pmic_status(void)
 {
 	void *fdt = get_dt_blob();
 
@@ -75,7 +75,7 @@ static int dt_pmic_status(void)
 
 static bool dt_pmic_is_secure(void)
 {
-	int status = dt_pmic_status();
+	int status = stm32mp_dt_pmic_status();
 
 	return ((unsigned)status == DT_STATUS_OK_SEC) &&
 	       (i2c_handle.dt_status == DT_STATUS_OK_SEC);

commit 0e7f6098e8dc4055f017f1ffb3499afa518d5c95

plat-stm32mp1: pm: fix inline function stm32mp_dt_pmic_status()
Fix definition of stm32mp_dt_pmic_status() when configueration
switch CFG_STPMIC1 is disabled.

Fixes: 9397a52cb17e ("pmic: stm32mp: export stm32mp_dt_pmic_status")
Change-Id: I9c3b4be7ae069b4583b8af683ecf61c4eec9cd28
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/155315
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Lionel DEBIEVE <lionel.debieve@st.com>

core/arch/arm/plat-stm32mp1/drivers/stm32mp1_pmic.h
@@ -29,7 +29,7 @@ static inline void stm32mp_put_pmic(void)
 {
 	panic();
 }
-int stm32mp_dt_pmic_status(void)
+static inline int stm32mp_dt_pmic_status(void)
 {
 	return -1;
 }