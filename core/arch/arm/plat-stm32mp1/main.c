zfeifei90/optee_os

commit 521c462a19f7fda6673a3886c3d034f87e86109c

stm32mp1: check OPP HW support
Information is extracted from part number, and it will be then used to
filter OPP table.
get_part_number() function computes the value from OTP content and
device ID read in DBGMCU.

Change-Id: I4e38a345658d995c521a8beb8e120a1e2f76e2d8
Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/150135
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Etienne CARRIERE <etienne.carriere@st.com>

core/arch/arm/plat-stm32mp1/main.c
@@ -361,19 +361,37 @@ bool sm_platform_handler(struct sm_ctx *ctx)
 	}
 }
 
+static uintptr_t stm32_dbgmcu_base(void)
+{
+	static void *va;
+
+	if (!cpu_mmu_enabled())
+		return DBGMCU_BASE;
+
+	if (!va)
+		va = phys_to_virt(DBGMCU_BASE, MEM_AREA_IO_SEC);
+
+	return (uintptr_t)va;
+}
+
 /* SoC versioning util */
 int stm32mp1_dbgmcu_get_chip_version(uint32_t *chip_version)
 {
-	uintptr_t base = DBGMCU_BASE;
-
 	assert(chip_version != NULL);
 
-	if (cpu_mmu_enabled()) {
-		base = (uintptr_t)phys_to_virt(DBGMCU_BASE, MEM_AREA_IO_SEC);
-	}
-
-	*chip_version = (read32(base + DBGMCU_IDC) &
+	*chip_version = (read32(stm32_dbgmcu_base() + DBGMCU_IDC) &
 			 DBGMCU_IDC_REV_ID_MASK) >> DBGMCU_IDC_REV_ID_SHIFT;
+
+	return 0;
+}
+
+/* SoC device ID util */
+int stm32mp1_dbgmcu_get_chip_dev_id(uint32_t *chip_dev_id)
+{
+	assert(chip_dev_id != NULL);
+
+	*chip_dev_id = read32(stm32_dbgmcu_base() + DBGMCU_IDC) &
+		       DBGMCU_IDC_DEV_ID_MASK;
 
 	return 0;
 }
@@ -569,6 +587,71 @@ unsigned long stm32_get_iwdg_otp_config(uintptr_t pbase)
 	}
 
 	return iwdg_cfg;
+}
+
+static int get_part_number(uint32_t *part_nb)
+{
+	uint32_t part_number;
+	uint32_t dev_id;
+	uint32_t otp;
+	uint32_t otp_value;
+	size_t bit_len;
+
+	assert(part_nb != NULL);
+
+	if (stm32mp1_dbgmcu_get_chip_dev_id(&dev_id) != 0) {
+		return -1;
+	}
+
+	if (bsec_find_otp_name_in_nvmem_layout(PART_NUMBER_OTP, &otp,
+					       &bit_len) != BSEC_OK) {
+		return -1;
+	}
+
+	assert(bit_len == 8);
+
+	if (bsec_read_otp(&part_number, otp) != BSEC_OK) {
+		return -1;
+	}
+
+	part_number = (part_number & PART_NUMBER_OTP_PART_MASK) >>
+		      PART_NUMBER_OTP_PART_SHIFT;
+
+	*part_nb = part_number | (dev_id << 16);
+
+	return 0;
+}
+
+bool stm32mp_supports_cpu_opp(uint32_t opp_id)
+{
+	uint32_t part_number;
+	uint32_t id;
+
+	if (get_part_number(&part_number) != 0) {
+		DMSG("Cannot get part number");
+		panic();
+	}
+
+	switch (opp_id) {
+	case PLAT_OPP_ID1:
+	case PLAT_OPP_ID2:
+		id = opp_id;
+		break;
+	default:
+		return false;
+	}
+
+	switch (part_number) {
+	case STM32MP157F_PART_NB:
+	case STM32MP157D_PART_NB:
+	case STM32MP153F_PART_NB:
+	case STM32MP153D_PART_NB:
+	case STM32MP151F_PART_NB:
+	case STM32MP151D_PART_NB:
+		return true;
+	default:
+		return id == PLAT_OPP_ID1;
+	}
 }
 
 uintptr_t stm32mp_get_etzpc_base(void)

core/arch/arm/plat-stm32mp1/platform_config.h
@@ -97,10 +97,14 @@
 #define OTP_MAX_SIZE			(STM32MP1_OTP_MAX_ID + 1U)
 
 #define HW2_OTP				"hw2_otp"
+#define PART_NUMBER_OTP			"part_number_otp"
 
 #define HW2_OTP_IWDG_HW_ENABLE_SHIFT	3
 #define HW2_OTP_IWDG_FZ_STOP_SHIFT	5
 #define HW2_OTP_IWDG_FZ_STANDBY_SHIFT	7
+
+#define PART_NUMBER_OTP_PART_MASK	GENMASK_32(7, 0)
+#define PART_NUMBER_OTP_PART_SHIFT	0
 
 /*
  * GPIO banks: 11 non secure banks (A to K) and 1 secure bank (Z)
@@ -142,13 +146,31 @@
 /* RCC platform resources */
 #define RCC_WAKEUP_IT			177
 
-/* SoC revision */
+/* SoC part numbers and revisions */
+#define STM32MP157C_PART_NB		0x05000000
+#define STM32MP157A_PART_NB		0x05000001
+#define STM32MP153C_PART_NB		0x05000024
+#define STM32MP153A_PART_NB		0x05000025
+#define STM32MP151C_PART_NB		0x0500002E
+#define STM32MP151A_PART_NB		0x0500002F
+#define STM32MP157F_PART_NB		0x05000080
+#define STM32MP157D_PART_NB		0x05000081
+#define STM32MP153F_PART_NB		0x050000A4
+#define STM32MP153D_PART_NB		0x050000A5
+#define STM32MP151F_PART_NB		0x050000AE
+#define STM32MP151D_PART_NB		0x050000AF
+
 #define STM32MP1_REV_A			0x00001000
 #define STM32MP1_REV_B			0x00002000
 #define STM32MP1_REV_Z			0x00002001
 
+/* OPP */
+#define PLAT_OPP_ID1			1U
+#define PLAT_OPP_ID2			2U
+
 /* DBGMCU resources */
 #define DBGMCU_IDC			0x0
+#define DBGMCU_IDC_DEV_ID_MASK		GENMASK_32(11, 0)
 #define DBGMCU_IDC_REV_ID_MASK		GENMASK_32(31, 16)
 #define DBGMCU_IDC_REV_ID_SHIFT		16
 

core/arch/arm/plat-stm32mp1/stm32_util.h
@@ -15,8 +15,12 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-/* SoC versioning */
+/* SoC versioning and device ID */
 int stm32mp1_dbgmcu_get_chip_version(uint32_t *chip_version);
+int stm32mp1_dbgmcu_get_chip_dev_id(uint32_t *chip_dev_id);
+
+/* OPP service */
+bool stm32mp_supports_cpu_opp(uint32_t opp_id);
 
 /* SiP & OEM platform services */
 bool stm32_sip_service(struct sm_ctx *ctx,

commit b5a1d20ff3cb5378beeed8566a9bac1c59d4aa7c

plat-stm32mp1: fix build warning in get_part_number()
Fix build error reported by GCC:

core/arch/arm/plat-stm32mp1/main.c:631:11: error: unused variable ‘otp_value’ [-Werror=unused-variable]
  uint32_t otp_value;
           ^~~~~~~~~
Fixes: 521c462a19f7 ("stm32mp1: check OPP HW support")
Change-Id: I02db4d202750ceda88dbb6027e6ed477bc5211f6
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/155309
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Lionel DEBIEVE <lionel.debieve@st.com>

core/arch/arm/plat-stm32mp1/main.c
@@ -622,7 +622,6 @@ static int get_part_number(uint32_t *part_nb)
 	uint32_t part_number;
 	uint32_t dev_id;
 	uint32_t otp;
-	uint32_t otp_value;
 	size_t bit_len;
 
 	assert(part_nb != NULL);