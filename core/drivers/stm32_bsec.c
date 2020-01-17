zfeifei90/optee_os

commit 9a97e4365b7ce8d7c6d40393f6b2286c2938f33b

bsec: add function to get OTP information in NVMEM layout data
bsec_find_otp_name_in_nvmem_layout() provides the characteristics
(ID and length) of an OTP from the device tree, if well-configured
inside BSEC node. Because Device Tree is only available from
pageable memory, save_dt_nvmem_layout() function is executed at
init, to store all OTP information in a static structure. It uses
the nvmem_layout cell information, to access to the BSEC child node
corresponding to the OTP. This data is then parsed later, to get
OTP data from its name.

Change-Id: If2feb6645af59624f55468f8c291f0d798a17535
Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/144318
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Etienne CARRIERE <etienne.carriere@st.com>

core/drivers/stm32_bsec.c
@@ -19,9 +19,12 @@
 
 #ifdef CFG_DT
 #include <libfdt.h>
+#include <string.h>
 #endif
 
-#define BSEC_COMPAT	"st,stm32mp15-bsec"
+#define BSEC_COMPAT			"st,stm32mp15-bsec"
+#define DT_NVMEM_LAYOUT_COMPAT		"st,stm32-nvmem-layout"
+
 #define BITS_PER_WORD	(CHAR_BIT * sizeof(uint32_t))
 #define OTP_ACCESS_SIZE	(ROUNDUP(OTP_MAX_SIZE, BITS_PER_WORD) / BITS_PER_WORD)
 
@@ -124,6 +127,96 @@ static int bsec_dt_otp_nsec_access(void *fdt, int bsec_node)
 
 	return 0;
 }
+
+struct nvmem_layout {
+	char *name;
+	uint32_t number;
+	size_t bit_len;
+};
+
+static struct nvmem_layout *nvmem_layout;
+static size_t nvmem_layout_count;
+
+static int save_dt_nvmem_layout(void *fdt)
+{
+	const fdt32_t *cells;
+	int i;
+	int cell_nb;
+	int nvmem_node;
+
+	nvmem_node = fdt_get_node_by_compatible(fdt, DT_NVMEM_LAYOUT_COMPAT);
+	if (nvmem_node < 0) {
+		return 0;
+	}
+
+	cells = fdt_getprop(fdt, nvmem_node, "nvmem-cells", &cell_nb);
+	if (cells == NULL) {
+		cell_nb = 0;
+	}
+
+	cell_nb /= sizeof(uint32_t);
+
+	i = fdt_stringlist_count(fdt, nvmem_node, "nvmem-cell-names");
+	if (i < 0) {
+		i = 0;
+	}
+
+	if (cell_nb != i) {
+		EMSG("Inconsistent NVMEM layout");
+		panic();
+	}
+
+	nvmem_layout = calloc(cell_nb, sizeof(*nvmem_layout));
+	if (nvmem_layout == NULL) {
+		panic();
+	}
+
+	nvmem_layout_count = (size_t)cell_nb;
+
+	for (i = 0; i < cell_nb; i++) {
+		const fdt32_t *cuint;
+		const char *string;
+		int len;
+		int node;
+
+		node = fdt_node_offset_by_phandle(fdt,
+						  fdt32_to_cpu(*(cells + i)));
+		if (node < 0) {
+			EMSG("Malformed nvmem_layout node: ignored");
+			continue;
+		}
+
+		cuint = fdt_getprop(fdt, node, "reg", &len);
+		if ((cuint == NULL) || (len != (2 * (int)sizeof(uint32_t))))  {
+			EMSG("Malformed nvmem_layout node: ignored");
+			continue;
+		}
+
+		if (fdt32_to_cpu(*cuint) % sizeof(uint32_t)) {
+			EMSG("Misaligned nvmem_layout element: ignored");
+			continue;
+		}
+
+		nvmem_layout[i].number = fdt32_to_cpu(*cuint) /
+					 sizeof(uint32_t);
+		nvmem_layout[i].bit_len = fdt32_to_cpu(*(cuint + 1)) * CHAR_BIT;
+
+		string = fdt_stringlist_get(fdt, nvmem_node, "nvmem-cell-names",
+					    i, &len);
+		if ((string == NULL) || (len == 0)) {
+			continue;
+		}
+
+		nvmem_layout[i].name = calloc(1, len + 1);
+		if (nvmem_layout[i].name == NULL) {
+			panic();
+		}
+
+		memcpy(nvmem_layout[i].name, string, len);
+	}
+
+	return 0;
+}
 #endif
 
 static uint32_t otp_bank_offset(uint32_t otp)
@@ -172,6 +265,48 @@ static uint32_t bsec_check_error(uint32_t otp, bool check_disturbed)
 
 	return BSEC_OK;
 }
+
+#ifdef CFG_DT
+/*
+ * bsec_find_otp_name_in_nvmem_layout: find and get OTP location from its name.
+ * name: sub-node name to look up.
+ * otp: pointer to read OTP number or NULL.
+ * otp_bit_len: pointer to read OTP length in bits or NULL.
+ * return value: BSEC_OK if no error.
+ */
+uint32_t bsec_find_otp_name_in_nvmem_layout(const char *name, uint32_t *otp,
+					    uint32_t *otp_bit_len)
+{
+	size_t i;
+	void *fdt;
+	int node;
+	int index, len;
+	const fdt32_t *cuint;
+
+	if (name == NULL) {
+		return BSEC_INVALID_PARAM;
+	}
+
+	for (i = 0; i < nvmem_layout_count; i++) {
+		if (!nvmem_layout[i].name ||
+		    strcmp(name, nvmem_layout[i].name)) {
+			continue;
+		}
+
+		if (otp != NULL) {
+			*otp = nvmem_layout[i].number;
+		}
+
+		if (otp_bit_len != NULL) {
+			*otp_bit_len = nvmem_layout[i].bit_len;
+		}
+
+		return BSEC_OK;
+	}
+
+	return BSEC_ERROR;
+}
+#endif
 
 /*
  * bsec_shadow_register: copy SAFMEM OTP to BSEC data.
@@ -761,6 +896,8 @@ static TEE_Result initialize_bsec(void)
 	}
 
 	bsec_dt_otp_nsec_access(fdt, node);
+
+	save_dt_nvmem_layout(fdt);
 
 	return TEE_SUCCESS;
 }

core/include/drivers/stm32_bsec.h
@@ -112,6 +112,19 @@
 #define FREQ_30_45_MHZ			0x2
 #define FREQ_45_67_MHZ			0x3
 
+#ifdef CFG_DT
+uint32_t bsec_find_otp_name_in_nvmem_layout(const char *name, uint32_t *otp,
+					    uint32_t *otp_bit_len);
+#else
+static inline uint32_t bsec_find_otp_name_in_nvmem_layout(
+						const char *name __unused,
+						uint32_t *otp __unused,
+						uint32_t *otp_bit_len __unused)
+{
+	return BSEC_ERROR;
+}
+#endif
+
 uint32_t bsec_shadow_register(uint32_t otp);
 uint32_t bsec_read_otp(uint32_t *val, uint32_t otp);
 uint32_t bsec_write_otp(uint32_t val, uint32_t otp);

commit e85075d849962754d39d982bbddafb061deebd3e

drivers: stm32_bsec: fix build warning
Fix build errors reported by GCC:

core/drivers/stm32_bsec.c:295:17: error: unused variable ‘cuint’ [-Werror=unused-variable]
  const fdt32_t *cuint;
                 ^~~~~
core/drivers/stm32_bsec.c:294:13: error: unused variable ‘len’ [-Werror=unused-variable]
  int index, len;
             ^~~
core/drivers/stm32_bsec.c:294:6: error: unused variable ‘index’ [-Werror=unused-variable]
  int index, len;
      ^~~~~
core/drivers/stm32_bsec.c:293:6: error: unused variable ‘node’ [-Werror=unused-variable]
  int node;
      ^~~~
core/drivers/stm32_bsec.c:292:8: error: unused variable ‘fdt’ [-Werror=unused-variable]
  void *fdt;
        ^~~
core/drivers/stm32_bsec.c: In function ‘bsec_read_otp’:
core/drivers/stm32_bsec.c:373:11: error: unused variable ‘exc’ [-Werror=unused-variable]
  uint32_t exc;
           ^~~

Fixes: 9a97e4365b7c ("bsec: add function to get OTP information in NVMEM layout data")
Change-Id: I7f2c3116d4b9e6fef28cb58b5ae67ac1a8c540ce
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/155262
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Lionel DEBIEVE <lionel.debieve@st.com>

core/drivers/stm32_bsec.c
@@ -289,10 +289,6 @@ uint32_t bsec_find_otp_name_in_nvmem_layout(const char *name, uint32_t *otp,
 					    uint32_t *otp_bit_len)
 {
 	size_t i;
-	void *fdt;
-	int node;
-	int index, len;
-	const fdt32_t *cuint;
 
 	if (name == NULL) {
 		return BSEC_INVALID_PARAM;
@@ -370,11 +366,8 @@ uint32_t bsec_shadow_register(uint32_t otp)
  */
 uint32_t bsec_read_otp(uint32_t *val, uint32_t otp)
 {
-	uint32_t exc;
-
-	if (otp > stm32mp_get_otp_max()) {
+	if (otp > stm32mp_get_otp_max())
 		return BSEC_INVALID_PARAM;
-	}
 
 	*val = read32(bsec_get_base() + BSEC_OTP_DATA_OFF +
 		      (otp * sizeof(uint32_t)));