zfeifei90/optee_os

commit 343a8da96cd0209712eb1be48bffb401d1ffebf0

iwdg: stm32: Fix OTP status management
Reverse bit definition as the OTP set is the IWDG
disabling. With this change, the platform device tree
and OTP configuration are consistent.

Change-Id: Id54aa9fa77381f61a540898f8055f1b69c3cad35
Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
Signed-off-by: Lionel Debieve <lionel.debieve@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/143540
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Etienne CARRIERE <etienne.carriere@st.com>

core/arch/arm/plat-stm32mp1/main.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright (c) 2017-2018, STMicroelectronics
+ * Copyright (c) 2017-2019, STMicroelectronics
  * Copyright (c) 2016-2018, Linaro Limited
  */
 
@@ -526,17 +526,21 @@ unsigned long stm32_get_iwdg_otp_config(uintptr_t pbase)
 
 	idx = stm32mp_iwdg_iomem2instance(pbase);
 
-	if (bsec_read_otp(&otp_value, HW2_OTP))
+	if (bsec_read_otp(&otp_value, HW2_OTP) != BSEC_OK) {
 		panic();
+	}
 
-	if (otp_value & BIT(idx + HW2_OTP_IWDG_HW_ENABLE_SHIFT))
+	if (otp_value & BIT(idx + HW2_OTP_IWDG_HW_ENABLE_SHIFT) != 0U) {
 		iwdg_cfg |= IWDG_HW_ENABLED;
+	}
 
-	if (!(otp_value & BIT(idx + HW2_OTP_IWDG_FZ_STOP_SHIFT)))
-		iwdg_cfg |= IWDG_ENABLE_ON_STOP;
+	if ((otp_value & BIT(idx + HW2_OTP_IWDG_FZ_STOP_SHIFT)) != 0U) {
+		iwdg_cfg |= IWDG_DISABLE_ON_STOP;
+	}
 
-	if (!(otp_value & BIT(idx + HW2_OTP_IWDG_FZ_STANDBY_SHIFT)))
-		iwdg_cfg |= IWDG_ENABLE_ON_STANDBY;
+	if ((otp_value & BIT(idx + HW2_OTP_IWDG_FZ_STANDBY_SHIFT)) != 0U) {
+		iwdg_cfg |= IWDG_DISABLE_ON_STANDBY;
+	}
 
 	return iwdg_cfg;
 }

core/drivers/stm32_iwdg.c
@@ -246,14 +246,14 @@ static TEE_Result iwdg_init(void)
 		iwdg.vbase = (uintptr_t)phys_to_virt(iwdg.pbase, memtype);
 
 		/* DT can specify low power cases */
-		if (fdt_getprop(fdt, node, "stm32,enable-on-stop", NULL) !=
+		if (fdt_getprop(fdt, node, "stm32,enable-on-stop", NULL) ==
 		    NULL) {
-			iwdg.flags |= IWDG_ENABLE_ON_STOP;
+			iwdg.flags |= IWDG_DISABLE_ON_STOP;
 		}
 
-		if (fdt_getprop(fdt, node, "stm32,enable-on-standby", NULL) !=
+		if (fdt_getprop(fdt, node, "stm32,enable-on-standby", NULL) ==
 		    NULL) {
-			iwdg.flags |= IWDG_ENABLE_ON_STANDBY;
+			iwdg.flags |= IWDG_DISABLE_ON_STANDBY;
 		}
 
 		hw_init = stm32_get_iwdg_otp_config(iwdg.pbase);
@@ -265,12 +265,12 @@ static TEE_Result iwdg_init(void)
 			iwdg.flags |= IWDG_HW_ENABLED;
 		}
 
-		if ((hw_init & IWDG_ENABLE_ON_STOP) != 0) {
-			iwdg.flags |= IWDG_ENABLE_ON_STOP;
+		if ((hw_init & IWDG_DISABLE_ON_STOP) != 0) {
+			iwdg.flags |= IWDG_DISABLE_ON_STOP;
 		}
 
-		if ((hw_init & IWDG_ENABLE_ON_STANDBY) != 0) {
-			iwdg.flags |= IWDG_ENABLE_ON_STANDBY;
+		if ((hw_init & IWDG_DISABLE_ON_STANDBY) != 0) {
+			iwdg.flags |= IWDG_DISABLE_ON_STANDBY;
 		}
 
 		if (dt_info.status == DT_STATUS_DISABLED) {

core/include/drivers/stm32_iwdg.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause */
 /*
- * Copyright (c) 2018, STMicroelectronics - All Rights Reserved
+ * Copyright (c) 2018-2019, STMicroelectronics - All Rights Reserved
  */
 
 #ifndef __STM32_IWDG_H__
@@ -9,8 +9,8 @@
 #include <stdint.h>
 
 #define IWDG_HW_ENABLED			BIT(0)
-#define IWDG_ENABLE_ON_STOP		BIT(1)
-#define IWDG_ENABLE_ON_STANDBY		BIT(2)
+#define IWDG_DISABLE_ON_STOP		BIT(1)
+#define IWDG_DISABLE_ON_STANDBY		BIT(2)
 
 void stm32_iwdg_refresh(uint32_t instance);
 

commit 36ce3f945987a85ab60b6fccc21e55b062270325

plat-stm32mp1: fix build warning in stm32_get_iwdg_otp_config()
Fix build error reported by GCC:

core/arch/arm/plat-stm32mp1/main.c:611:58: error: suggest parentheses around comparison in operand of ‘&’ [-Werror=parentheses]
  if (otp_value & BIT(idx + HW2_OTP_IWDG_HW_ENABLE_SHIFT) != 0U) {

This change also simplifies a bit the function implementation removing
unneeded parentheses and braces.

Change-Id: I9e96788e82bc15629ecf2326519906a2a5dcf2bb
Fixes: 343a8da96cd0 ("iwdg: stm32: Fix OTP status management")
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/155308
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Lionel DEBIEVE <lionel.debieve@st.com>

core/arch/arm/plat-stm32mp1/main.c
@@ -599,26 +599,20 @@ unsigned long stm32_get_iwdg_otp_config(uintptr_t pbase)
 
 	idx = stm32mp_iwdg_iomem2instance(pbase);
 
-	if (bsec_find_otp_name_in_nvmem_layout(HW2_OTP, &otp, NULL) !=
-	    BSEC_OK) {
+	if (bsec_find_otp_name_in_nvmem_layout(HW2_OTP, &otp, NULL))
 		panic();
-	}
 
-	if (bsec_read_otp(&otp_value, otp) != BSEC_OK) {
+	if (bsec_read_otp(&otp_value, otp))
 		panic();
-	}
 
-	if (otp_value & BIT(idx + HW2_OTP_IWDG_HW_ENABLE_SHIFT) != 0U) {
+	if (otp_value & BIT(idx + HW2_OTP_IWDG_HW_ENABLE_SHIFT))
 		iwdg_cfg |= IWDG_HW_ENABLED;
-	}
 
-	if ((otp_value & BIT(idx + HW2_OTP_IWDG_FZ_STOP_SHIFT)) != 0U) {
+	if (otp_value & BIT(idx + HW2_OTP_IWDG_FZ_STOP_SHIFT))
 		iwdg_cfg |= IWDG_DISABLE_ON_STOP;
-	}
 
-	if ((otp_value & BIT(idx + HW2_OTP_IWDG_FZ_STANDBY_SHIFT)) != 0U) {
+	if (otp_value & BIT(idx + HW2_OTP_IWDG_FZ_STANDBY_SHIFT))
 		iwdg_cfg |= IWDG_DISABLE_ON_STANDBY;
-	}
 
 	return iwdg_cfg;
 }