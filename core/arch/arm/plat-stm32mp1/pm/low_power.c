zfeifei90/optee_os

commit 62318247008c3aa13a577ffdf96baf66ea6731e0

stm32mp1: independent reset: fix shutdown sequence
Reset only the non-current core in shutdown sequence.
Do a System Reset in CSTOP RESET case.
Clear context Magic Number to initialize correctly
the DDR controller on the next boot.

Change-Id: I8d0d8427956de5ae48e3ef3125d5f11a602e2581
Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>
Reviewed-on: https://gerrit.st.com/127941
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Etienne CARRIERE <etienne.carriere@st.com>

core/arch/arm/plat-stm32mp1/pm/low_power.c
@@ -326,6 +326,25 @@ void __noreturn stm32_cores_reset(void)
 }
 KEEP_PAGER(stm32_cores_reset);
 
+static void reset_other_core(void)
+{
+	uintptr_t rcc_base = stm32_rcc_base();
+	uint32_t reset_mask;
+	uint32_t target_mask;
+
+	if (get_core_pos() == 0) {
+		reset_mask = RCC_MP_GRSTCSETR_MPUP1RST;
+		target_mask = TARGET_CPU1_GIC_MASK;
+	} else {
+		reset_mask = RCC_MP_GRSTCSETR_MPUP0RST;
+		target_mask = TARGET_CPU0_GIC_MASK;
+	}
+
+	itr_raise_sgi(GIC_SEC_SGI_1, target_mask);
+
+	write32(reset_mask, rcc_base + RCC_MP_GRSTCSETR);
+}
+
 /*
  * stm32_enter_cstop_shutdown - Shutdown CPUs to target low power mode
  * @mode - Target low power mode
@@ -345,9 +364,13 @@ void __noreturn stm32_enter_cstop_shutdown(uint32_t mode)
 	case STM32_PM_CSTOP_ALLOW_STANDBY_DDR_OFF:
 #ifdef STM32MP1_USE_MPU0_RESET
 		stm32mp_pm_shutdown_context();
+		reset_other_core();
 		stm32_enter_cstop(mode);
-		cpu_wfi();
-		reset_cores();
+		dsb();
+		isb();
+		for ( ; ; ) {
+			wfi();
+		}
 #else
 		if (stm32mp_with_pmic()) {
 			wait_console_flushed();
@@ -378,14 +401,10 @@ void __noreturn stm32_enter_cstop_reset(uint32_t mode)
 		udelay(100);
 		break;
 	default:
-#ifdef STM32MP1_USE_MPU0_RESET
-		reset_cores();
-#else
 		IMSG("Forced system reset");
 		wait_console_flushed();
 		write32(RCC_MP_GRSTCSETR_MPSYSRST, rcc_base + RCC_MP_GRSTCSETR);
 		udelay(100);
-#endif
 		break;
 	}
 

core/arch/arm/plat-stm32mp1/pm/context.c
@@ -425,21 +425,25 @@ static void save_teeram_in_ddr(void)
 static void enable_pm_mailbox(unsigned int suspend)
 {
 	struct pm_mailbox *mailbox = get_pm_mailbox();
-	uint32_t magic = BOOT_API_A7_CORE0_MAGIC_NUMBER;
+	uint32_t magic = 0;
 	uint32_t hint = 0;
 
 	assert(stm32_clock_is_enabled(BKPSRAM) &&
 	       stm32_clock_is_enabled(RTCAPB));
 
 	if (suspend) {
+		magic = BOOT_API_A7_CORE0_MAGIC_NUMBER;
+		mailbox->magic = STANDBY_CONTEXT_MAGIC;
+
 		hint = virt_to_phys(&get_retram_resume_ctx()->resume_sequence);
+	} else {
+		mailbox->magic = 0;
 	}
 
 	write32(magic, stm32mp_bkpreg(BCKR_CORE1_MAGIC_NUMBER));
 	write32(hint, stm32mp_bkpreg(BCKR_CORE1_BRANCH_ADDRESS));
 
 	mailbox->core0_resume_ep = hint;
-	mailbox->magic = STANDBY_CONTEXT_MAGIC;
 }
 
 static void gate_pm_context_clocks(bool enable)

commit 71529813bb28cf4f782263c9f22ef7f5dcd67f16

plat-stm32mp1: pm: fix build warning for reset_other_core()
Fix build errors reported by GCC:

core/arch/arm/plat-stm32mp1/pm/low_power.c:423:13: error: ‘reset_other_core’ defined but not used [-Werror=unused-function]
 static void reset_other_core(void)
         ^~~~~~~~~~~~~~~~

Fixes: 62318247008c ("stm32mp1: independent reset: fix shutdown sequence")
Change-Id: I436053e7b3d7176f0e0c73195b43f1afe813e961
Signed-off-by: Etienne Carriere <etienne.carriere@st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/155314
Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
Reviewed-by: Lionel DEBIEVE <lionel.debieve@st.com>

core/arch/arm/plat-stm32mp1/pm/low_power.c
@@ -420,7 +420,7 @@ void __noreturn stm32_cores_reset(void)
 }
 KEEP_PAGER(stm32_cores_reset);
 
-static void reset_other_core(void)
+static void __maybe_unused reset_other_core(void)
 {
 	uintptr_t rcc_base = stm32_rcc_base();
 	uint32_t reset_mask;