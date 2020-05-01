8eb9ef24d64726fcca923db6974c40546e735f4a
Etienne Carriere <etienne.carriere@st.com>
2020-05-01 14:52:28 +0200

    plat-stm32mp1: enhance trace message on security hardening
    Change trace message to state when RCC security is hardened or not.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- core/arch/arm/plat-stm32mp1/shared_resources.c
+++ core/arch/arm/plat-stm32mp1/shared_resources.c
@@ -651,27 +651,36 @@ static void check_rcc_secure_configuration(void)
 {
 	bool secure = stm32_rcc_is_secure();
 	bool mckprot = stm32_rcc_is_mckprot();
+	bool need_secure = false;
+	bool need_mckprot = false;
 	enum stm32mp_shres id = STM32MP1_SHRES_COUNT;
-	bool have_error = false;
 
-	if (stm32mp_is_closed_device() && !secure)
-		panic();
+	if (stm32mp_is_closed_device() && !secure) {
+		DMSG("stm32mp1: closed device mandates OP-TEE to secure RCC");
+		need_secure = true;
+	}
 
 	for (id = 0; id < STM32MP1_SHRES_COUNT; id++) {
 		if  (shres_state[id] != SHRES_SECURE)
 			continue;
 
-		if ((mckprot_resource(id) && !mckprot) || !secure) {
-			EMSG("RCC %s MCKPROT %s and %s (%u) secure",
-			      secure ? "secure" : "non-secure",
-			      mckprot ? "set" : "not set",
-			      shres2str_id(id), id);
-			have_error = true;
-		}
+		need_secure = true;
+		if (mckprot_resource(id))
+			need_mckprot = true;
+
+		if ((mckprot_resource(id) && !mckprot) || !secure)
+			EMSG("Error: RCC TZEN=%u MCKPROT=%u while %s is secure",
+			      secure, mckprot, shres2str_id(id));
 	}
 
-	if (have_error)
+	DMSG("RCC/PWR secure hardening: TZEN %sable, MCKPROT %sable",
+	     secure ? "en" : "dis", mckprot ? "en" : "dis");
+
+	if (need_secure && !secure)
 		panic();
+
+	if (need_mckprot)
+	        io_setbits32(stm32_rcc_base() + RCC_TZCR, RCC_TZCR_MCKPROT);
 }
 
 static void set_gpio_secure_configuration(void)
