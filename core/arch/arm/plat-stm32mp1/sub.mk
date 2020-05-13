787e072f1272a9290b8541027a8771e34644ac69
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2020-05-13 13:47:44 +0200

    core: stm32mp1: add remoteproc pseudo TA
    Add pseudo TA that manages the cortex M4 firmware for the
    stm32mp1 machine. This PTA is in charge of loading the sections
    of the firmware in MCU RAMs and the management of the firmware life cycle.
    
    Change-Id: I4dd9f37e82b7172b72f0218df57d86f18f649c6f
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>

--- core/arch/arm/plat-stm32mp1/sub.mk
+++ core/arch/arm/plat-stm32mp1/sub.mk
@@ -6,6 +6,7 @@ srcs-$(CFG_STM32_RNG) += rng_seed.c
 srcs-y += scmi_server.c
 srcs-y += shared_resources.c
 srcs-$(CFG_TZC400) += plat_tzc400.c
+srcs-$(CFG_RPROC_PTA) += remoteproc_pta.c
 
 subdirs-y += drivers
 subdirs-y += nsec-service
