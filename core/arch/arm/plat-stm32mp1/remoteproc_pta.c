4b9123a1ca8f39de059c1bd4a9a417257d6bf2b9
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2020-09-16 18:47:00 +0200

    core: stm32mp1: implement the PTA_REMOTEPROC_VERIFY_SHA256_DIGEST API
    Implement the digest verification based on the remoteproc signature.
    To not transmit the signature to the TA, the verification is
    implemented in the PTA.
    In future, rproc TA could access the key through OP-TEE secure storage.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>
    Change-Id: I28804a6fb9c9896ee8ca39ada0c66ff85d042c33

--- core/arch/arm/plat-stm32mp1/remoteproc_pta.c
+++ core/arch/arm/plat-stm32mp1/remoteproc_pta.c
@@ -13,6 +13,7 @@
 #include <mm/core_memprot.h>
 #include <mm/core_mmu.h>
 #include <remoteproc_pta.h>
+#include <rproc_pub_key.h>
 #include <stm32_util.h>
 #include <string.h>
 
@@ -385,6 +386,72 @@ static TEE_Result rproc_pta_stop(uint32_t pt,
 	return TEE_SUCCESS;
 }
 
+static TEE_Result rproc_pta_verify_rsa_signature(TEE_Param *hash,
+						 TEE_Param *sig, uint32_t algo)
+{
+	struct rsa_public_key key = { };
+	TEE_Result res = TEE_ERROR_GENERIC;
+	uint32_t e = TEE_U32_TO_BIG_ENDIAN(rproc_pub_key_exponent);
+	size_t hash_size = (size_t)hash->memref.size;
+	size_t sig_size = (size_t)sig->memref.size;
+
+	res = crypto_acipher_alloc_rsa_public_key(&key, sig_size);
+	if (res)
+		return TEE_ERROR_SECURITY;
+
+	res = crypto_bignum_bin2bn((uint8_t *)&e, sizeof(e), key.e);
+	if (res)
+		goto out;
+
+	res = crypto_bignum_bin2bn(rproc_pub_key_modulus,
+				   rproc_pub_key_modulus_size, key.n);
+	if (res)
+		goto out;
+
+	res = crypto_acipher_rsassa_verify(algo, &key, hash_size,
+					   hash->memref.buffer, hash_size,
+					   sig->memref.buffer, sig_size);
+
+out:
+	crypto_acipher_free_rsa_public_key(&key);
+
+	return res;
+}
+
+static TEE_Result rproc_pta_verify_digest(uint32_t pt,
+					  TEE_Param params[TEE_NUM_PARAMS])
+{
+	struct rproc_pta_key_info *keyinfo = NULL;
+	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_VALUE_INPUT,
+						TEE_PARAM_TYPE_MEMREF_INPUT,
+						TEE_PARAM_TYPE_MEMREF_INPUT,
+						TEE_PARAM_TYPE_MEMREF_INPUT);
+
+	if (pt != exp_pt)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	/* Only STM32_M4_FW_ID supported */
+	if (params[0].value.a != STM32_M4_FW_ID) {
+		EMSG("Unsupported firmware ID %#"PRIx32, params[0].value.a);
+		return TEE_ERROR_NOT_SUPPORTED;
+	}
+
+	if (rproc_ta_state != REMOTEPROC_OFF)
+		return TEE_ERROR_BAD_STATE;
+
+	keyinfo = params[1].memref.buffer;
+
+	if (!keyinfo ||
+	    RPROC_PTA_GET_KEYINFO_SIZE(keyinfo) != params[1].memref.size)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	if (keyinfo->algo != TEE_ALG_RSASSA_PKCS1_V1_5_SHA256)
+		return TEE_ERROR_NOT_SUPPORTED;
+
+	return rproc_pta_verify_rsa_signature(&params[2], &params[3],
+					      keyinfo->algo);
+}
+
 static TEE_Result rproc_pta_invoke_command(void *pSessionContext __unused,
 					   uint32_t cmd_id,
 					   uint32_t param_types,
@@ -403,6 +470,8 @@ static TEE_Result rproc_pta_invoke_command(void *pSessionContext __unused,
 		return rproc_pta_stop(param_types, params);
 	case PTA_REMOTEPROC_FIRMWARE_DA_TO_PA:
 		return rproc_pta_da_to_pa(param_types, params);
+	case PTA_REMOTEPROC_VERIFY_DIGEST:
+		return rproc_pta_verify_digest(param_types, params);
 	default:
 		break;
 	}
