df93c7316e304bb6fea71a48974a13fea4fc6da1
Etienne Carriere <etienne.carriere@linaro.org>
2021-02-11 15:28:54 +0100

    core: add tlbi_mva_range_asid()
    Adds tlbi_mva_range_asid() which invalidates a range of virtual
    addresses for a specific ASID.
    
    Change-Id: Icd123b2d8d65001cfe9edb0da862bbb0a7e273bb
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    [etienne: rebased]
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/194252
    Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Lionel DEBIEVE <lionel.debieve@foss.st.com>
    Tested-by: Lionel DEBIEVE <lionel.debieve@foss.st.com>

--- core/arch/arm/include/mm/core_mmu.h
+++ core/arch/arm/include/mm/core_mmu.h
@@ -613,6 +613,18 @@ enum teecore_tlb_op {
 /* TLB invalidation for a range of virtual address */
 void tlbi_mva_range(vaddr_t va, size_t size, size_t granule);
 
+/*
+ * tlbi_mva_range_asid() - Invalidate TLB for virtual address range for
+ *			   a specific ASID
+ * @va:		start virtual address, must be a multiple of @granule
+ * @len:	length in bytes of range, must be a multiple of @granule
+ * @granule:	granularity of mapping, supported values are
+ *		CORE_MMU_PGDIR_SIZE or SMALL_PAGE_SIZE. This value must
+ *		match the actual mappings.
+ * @asid:	Address space identifier
+ */
+void tlbi_mva_range_asid(vaddr_t va, size_t len, size_t granule, uint32_t asid);
+
 /* deprecated: please call straight tlbi_all() and friends */
 int core_tlb_maintenance(int op, unsigned long a) __deprecated;
 
