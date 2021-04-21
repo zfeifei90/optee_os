fc635660a11e5d46bb91158d0ad4baad94b8f2a7
Etienne Carriere <etienne.carriere@linaro.org>
2021-04-21 09:20:13 +0200

    core: include: common stm32mp_dt_bindings.h header file
    Hide DT bindings header file path in a common file.
    
    Change-Id: Ie2022c71e60704c38bd76e65086363840713897e
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/209657
    Tested-by: Lionel DEBIEVE <lionel.debieve@foss.st.com>
    Reviewed-by: Lionel DEBIEVE <lionel.debieve@foss.st.com>

---
+++ core/include/drivers/stm32mp_dt_bindings.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2021, STMicroelectronics
+ */
+#ifndef STM32MP_DT_BINDINGS_H
+#define STM32MP_DT_BINDINGS_H
+
+#ifdef CFG_STM32MP13
+#include <dt-bindings/clock/stm32mp13-clks.h>
+#include <dt-bindings/clock/stm32mp13-clksrc.h>
+#include <dt-bindings/reset/stm32mp13-resets.h>
+#include <dt-bindings/soc/stm32mp13-etzpc.h>
+#endif
+
+#ifdef CFG_STM32MP15
+#include <dt-bindings/clock/stm32mp1-clks.h>
+#include <dt-bindings/clock/stm32mp1-clksrc.h>
+#include <dt-bindings/reset/stm32mp1-resets.h>
+#include <dt-bindings/soc/stm32mp15-etzpc.h>
+#endif
+
+#endif /* STM32MP_DT_BINDINGS_H */
