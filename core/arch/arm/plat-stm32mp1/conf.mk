653d1be2d8c08df5de16b34386aad21354335478
Lionel Debieve <lionel.debieve@foss.st.com>
2021-11-02 23:00:00 +0100

    plat-stm32mp1: add a PTA for BSEC interface
    Add a new BSEC interface for fuse access from TA or REE.
    Disable the SIP interface to access fuses.
    
    Signed-off-by: Lionel Debieve <lionel.debieve@foss.st.com>
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Change-Id: I5842b4e3f4080f42e86f06a39299b4c9dcadfe4e

--- core/arch/arm/plat-stm32mp1/conf.mk
+++ core/arch/arm/plat-stm32mp1/conf.mk
@@ -148,6 +148,12 @@ else
 $(call force,CFG_DRIVERS_CLK_DT,y)
 endif
 
+# Enable BSEC Pseudo TA for fuses access management
+CFG_BSEC_PTA ?= y
+ifeq ($(CFG_BSEC_PTA),y)
+$(call force,CFG_STM32_BSEC,y,Mandated by CFG_BSEC_PTA)
+endif
+
 CFG_STM32_BSEC ?= y
 CFG_STM32_CRYP ?= y
 CFG_STM32_ETZPC ?= y
@@ -177,7 +183,7 @@ endif
 CFG_STM32MP_PANIC_ON_TZC_PERM_VIOLATION ?= y
 
 # SiP/OEM service for non-secure world
-CFG_STM32_BSEC_SIP ?= y
+CFG_STM32_BSEC_SIP ?= n
 CFG_STM32MP1_SCMI_SIP ?= y
 ifeq ($(CFG_STM32MP1_SCMI_SIP),y)
 $(call force,CFG_SCMI_MSG_DRIVERS,y,Mandated by CFG_STM32MP1_SCMI_SIP)
