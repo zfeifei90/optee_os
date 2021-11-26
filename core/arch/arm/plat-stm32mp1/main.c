7d62687885e686daf44ddc81378f0a38cff91a3b
Lionel Debieve <lionel.debieve@foss.st.com>
2021-11-26 01:24:53 +0100

    plat-stm32mp1: update the APB buses for STM32MP13 platform
    Update the physical buses and length for the STM32MP13 platform.
    
    Signed-off-by: Lionel Debieve <lionel.debieve@foss.st.com>
    Change-Id: I67d7aadb78362f516a06473d189a5c0146c4c3ed

--- core/arch/arm/plat-stm32mp1/main.c
+++ core/arch/arm/plat-stm32mp1/main.c
@@ -32,8 +32,12 @@ register_phys_mem_pgdir(MEM_AREA_IO_NSEC, APB5_BASE, APB5_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_NSEC, AHB4_BASE, AHB4_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_NSEC, AHB5_BASE, AHB5_SIZE);
 
-register_phys_mem_pgdir(MEM_AREA_IO_SEC, APB3_BASE, APB4_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, APB3_BASE, APB3_SIZE);
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, APB4_BASE, APB4_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, APB5_BASE, APB5_SIZE);
+#ifdef CFG_STM32MP13
+register_phys_mem_pgdir(MEM_AREA_IO_SEC, APB6_BASE, APB6_SIZE);
+#endif
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, AHB4_BASE, AHB4_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, AHB5_BASE, AHB5_SIZE);
 register_phys_mem_pgdir(MEM_AREA_IO_SEC, GIC_BASE, GIC_SIZE);
